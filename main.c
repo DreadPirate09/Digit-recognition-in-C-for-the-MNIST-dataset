beaa91018c9d4b6f56e741c6d1c1cb3d183ae01b
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2023-08-03 14:43:40 +0300

    implemented ReLU,softmax,sumExp,oneHot,getTrasponse and others

---
+++ main.c
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+float **giveMeZeros(int n, int m){
+	float** zeros = (float**)malloc(sizeof(float*)*n);
+	for(int i=0;i<n;i++){
+		zeros[i] = (float*)malloc(sizeof(float)*m);
+		for(int j=0;j<m;j++){
+			zeros[i][j] = 0;
+		}
+	}
+	return zeros;
+}
+
+float maxValueFromArray(float *array, int len){
+	float max = 0;
+	for(int i=0;i<len;i++){
+		if(max < array[i])
+			max = array[i];
+	}
+	printf("the max value from array with the len %d is %lf\n",len,max);
+	return max;
+}
+
+float **giveMeAMatrixNM(int n, int m){
+	float ** matrix = (float**)malloc(n*sizeof(float*));
+	for(int i=0;i<m;i++){
+		matrix[i] = (float*)malloc(m*sizeof(float));
+	}
+	return matrix;
+}
+
+float *giveMeArrayFromStack(float *array,int len){
+	float* dynamicArray = (float*)malloc(sizeof(float)*len);
+	for(int i=0;i<len;i++){
+		dynamicArray[i] = array[i];
+	}
+	return dynamicArray;
+}
+
+void printArray(float *array, int len){
+	for(int i=0;i<len;i++){
+		printf("%lf ",array[i]);
+	}
+}
+
+void printMatrix(float **matrix, int n, int m){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			printf("%.2lf ",matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+int* ReLU(float *array, int len){
+	float *new = (float*)malloc(len*sizeof(float));
+	for(int i = 0; i<len;i++){
+		if(array[i] > 0.0){
+			new[i] = array[i];
+		}else{
+			new[i] = 0.0;
+		}
+	}
+	return new;
+}
+
+float sumExp(float *array, int len){
+	float summed = 0;
+	for(int i=0;i<len;i++){
+		summed = summed + exp(array[i]);
+	}
+	return summed;
+}
+
+float* softMax(float *array, int len){
+	float *new = (float*)malloc(len*sizeof(float));
+	for(int i=0;i<len;i++){
+		new[i] = exp((float)array[i]) / sumExp(array,len);
+	}
+	return new;
+}
+
+float** getTranspose(float **matrix, int n, int m){
+	float **transpose = (float**)malloc(m*sizeof(float *));
+	for(int i=0;i<m;i++){
+		transpose[i] = (float*)malloc(n*sizeof(float));
+		for(int j=0;j<n;j++){
+			transpose[i][j] = matrix[j][i];
+		}
+	}
+	return transpose;
+}
+
+float** oneHot(float* Y, int len){
+	float** one_hot_y = giveMeZeros(len, maxValueFromArray(Y, len));
+	for(int i=0;i<len;i++){
+		one_hot_y[i][(int)Y[i]] = 1;
+	}
+	return getTranspose(one_hot_y,len,len);
+}
+
+int main(){
+
+	float array[10] = {0.63,-0.41,-0.22,0.53,-0.14,0.61,0.82,-0.22,-0.11,-0.32};
+	float xxarray[10] = {1,4,3,2,5,6,7,8,9,0};
+	float *dyn = giveMeArrayFromStack(xxarray,10);
+
+	float *x = ReLU(array,10);
+	float *softmax = softMax(array, 10);
+
+	// printArray(softmax, 10);
+
+	float **arrayMatrix = giveMeAMatrixNM(2,3);
+	for(int i=0;i<2;i++){
+		for(int j=0;j<3;j++){
+			arrayMatrix[i][j] = j;
+		}
+	}
+
+	// printMatrix(arrayMatrix, 2,3);
+	// printMatrix(getTranspose(arrayMatrix,1,3),3,1);
+
+	// printMatrix(giveMeZeros(31,33),31,33);
+
+	printMatrix(oneHot(dyn,10),10,10);
+	
+	
+
+	return 0;
+}
